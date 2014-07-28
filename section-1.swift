f4471e0c912c5de5033c2e518d06760c5a89f662
phonnz <agonzalez@nyxtechnology.com>
2014-07-28 09:58:37 -0500

    Closures

--- section-1.swift
+++ section-1.swift
@@ -1,54 +1,118 @@
 // Playground - noun: a place where people can play
+// @phonnz
 
 import UIKit
 
-//mutable variables
+
+// Variables dont need type
 var str = "Hello, playground"
 str += " of phonnz"
 
-var number = 4_000_44_4
+//Syntax for easy reading
+var number = 4_000_444
 
+// Array
 var ninjas = ["Hugo", "Andres", "Jorge", "phonnz", "luis", "Mario"]
 
+//if
+if  "a" == "a"
+{
+        println("Same character")
+}
+
+//loop
 for n in ninjas{
     println(n)
 }
 
+//Adding to the array
 ninjas += "unknow"
-ninjas
+
+//replace multitple elements on Array
 ninjas[3...5] = ["nuevo"]
 ninjas
+//Adding to the array
 ninjas.append("otro")
-ninjas
+
+//Remove elements on array
 ninjas.removeAtIndex(6)
 ninjas
+//Adding elements in a specific index
 ninjas.insert("nuevito", atIndex: 2)
 ninjas
 
+//Fill arrays
+var toFill = [String](count: 5, repeatedValue: "hola")
+toFill
 
-var rellenar = [String](count: 5, repeatedValue: "hola")
-rellenar
+//dictionary
+var dictionary = ["uno":1,"dos":2,"tres":3]
+dictionary["dos"]
 
-
-var dictionario = ["uno":1,"dos":2,"tres":3]
-
-for(key,valor) in dictionario{
-    println("llave \(key) y valor \(valor)")
+//loop key value
+for(key,value) in dictionary{
+    println("key: \(key) and value \(value)")
 }
 
-
+//let for unmutable variables
 let finalSquare = 25
 
+//Fill array with same value
 var consecutivo = [Int](count: finalSquare + 1, repeatedValue: 0)
-consecutivo
 
-func diHola(nombre: String)->String{
-    let saludo = "Hola " + nombre
+//Function
+func sayHello(name: String)->String{
+    let saludo = "Hola " + name
     return saludo
 }
+//Call the function
+println(sayHello("phonnz"))
 
-println(diHola("phonnz"))
+//joining strings
+func join(cadena vs1:String, cadena2 vs2: String, usando var3:String = "") -> String{
+    return vs1 + var3 + vs2
+}
+join(cadena: "inicial", cadena2:"segunda", usando: "-")
+
+//functional function
+func adding(a:Int, b:Int) -> Int{
+    return a + b
+}
 
 
+func multiply(a:Int, b:Int) -> Int{
+    return a * b
+}
+
+//mutable function
+var tempFunc : (Int,Int) -> Int = adding
+
+var result  = tempFunc(2,3)
+tempFunc = multiply
+
+tempFunc(2,3)
+
+
+
+//Closures
+//You can use additional variable names to explain the function params
+func increment(incrementCount count: Int) -> () -> Int{
+    var run = 0
+    //function into a function
+    func enhancer() -> Int{
+        run += count
+        return run
+    }
+    
+    return enhancer
+}
+
+
+let incrementa10 = increment(incrementCount: 10)
+let incrementa20 = increment(incrementCount: 20)
+
+
+incrementa20()
+incrementa10()
 
 
